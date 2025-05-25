fd3abedc4757254d61d7f8560eb5b47625fcb8f6
Shivam Bhilarkar <shivam.bhilarkar@Kinetixtt.com>
2025-05-25 14:14:34 +0530

    django templates

--- basic-project/tut_app_1/urls.py
+++ basic-project/tut_app_1/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.welcome_page, name="welcome_page"),
     path("home/", views.home_page, name="home_page"),
+    path("questions/", views.questions_page, name="question_page")
 ]
--- basic-project/tut_app_1/views.py
+++ basic-project/tut_app_1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Question
 # Create your views here.
 
 def welcome_page(request):
@@ -12,3 +13,8 @@ def home_page(request):
     # load the html template
     home_page_template = loader.get_template("tut_app_1/index.html")
     return HttpResponse(home_page_template.render(context={} , request= request))
+
+def questions_page(request):
+    question_page_template = loader.get_template("tut_app_1/questions.html")
+    all_questions = Question.objects.all()
+    return HttpResponse(question_page_template.render(context={ 'questions' : all_questions}, request= request))
